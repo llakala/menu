01a4b1c320f7500733e62bdb617811575f76978e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-04 14:45:17 -0400

    packages: define `revive` locally
    First step in the journey to removing my `llakaLib` dependency.

---
+++ packages/revive/package.nix
@@ -0,0 +1,14 @@
+{ llakaLib }:
+
+llakaLib.writeFishApplication {
+  name = "revive"; # Reuse Environment Variable If Value Encountered
+
+  text = /* fish */ ''
+    for val in $argv
+        if [ -n "$val" ]
+            echo $val
+            break
+        end
+    end
+  '';
+}

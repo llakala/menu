6adaa7616bad1024fc51a0702e524f9c93847970
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-04 14:40:59 -0400

    packages: use `builtins.attrValues` for composition

--- packages/rbld/package.nix
+++ packages/rbld/package.nix
@@ -1,32 +1,20 @@
 { pkgs, llakaLib, llakaPackages, lib, localPackages }:
 
-let
-  lixos-rebuild-ng = pkgs.nixos-rebuild-ng.override {
-    nix = pkgs.lix;
+assert lib.assertMsg (pkgs ? nixos-rebuild-ng) ''
+  RBLD relies on nixos-rebuild-ng, but it wasn't found in pkgs.
+  This likely means you made Menu follow an older nixpkgs version, which doesn't have nixos-rebuild-ng.
+  Instead, have Menu follow a more recent nixpkgs version.
+'';
+
+llakaLib.writeFishApplication {
+  name = "rbld"; # Rebuild But Less Dumb
+
+  runtimeInputs = builtins.attrValues {
+    inherit (pkgs) nix-output-monitor git;
+    nixos-rebuild = pkgs.nixos-rebuild-ng.override { nix = pkgs.lix; };
+    inherit (localPackages) hue;
+    inherit (llakaPackages) revive;
   };
 
-  nixpkgsDeps = with pkgs; [
-    nix-output-monitor # aka nom
-    git
-  ] ++ lib.singleton lixos-rebuild-ng;
-
-  selfDeps = with localPackages; [
-    hue
-  ];
-
-  llakaDeps = with llakaPackages; [
-    revive
-  ];
-
-in
-  assert lib.assertMsg (pkgs ? nixos-rebuild-ng) ''
-    RBLD relies on nixos-rebuild-ng, but it wasn't found in pkgs.
-    This likely means you made Menu follow nixpkgs 24.11, which doesn't have nixos-rebuild-ng.
-    Instead, have Menu follow nixpkgs-unstable.
-  '';
-  llakaLib.writeFishApplication {
-    name = "rbld"; # Rebuild But Less Dumb
-    runtimeInputs = nixpkgsDeps ++ selfDeps ++ llakaDeps;
-
-    text = builtins.readFile ./rbld.fish;
-  }
+  text = builtins.readFile ./rbld.fish;
+}

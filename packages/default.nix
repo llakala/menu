49e15cd90f6338701e590ac577a55f4fd7e2c73d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-05 10:08:53 -0400

    flake: remove llakaLib, add non-flake entrypoint
    With this, we finally no longer rely on llakaLib for anything - all our
    functions are defined locally.
    
    We also simplify the package definition process as much as possible.
    While `lib.filesystem.packagesFromDirectoryRecursive` is very nice for
    UX, it's a lot more of a black box. If we're okay with manually listing
    the files, we can achieve similar logic, with only `callPackageWith` and
    a little `let` recursion.
    
    Finally, non-flake users are now first-class citizens! Just pass an
    instantiated `pkgs` set into `./packages/default.nix`, and you'll get
    all the features flake users would get. This should really be documented
    in the README - but I'll leave that to future me.

---
+++ packages/default.nix
@@ -0,0 +1,18 @@
+{ pkgs }:
+
+let
+  # See https://nix.dev/tutorials/callpackage.html#interdependent-package-sets
+  # Lets packages rely on each other easily
+  callPackage = pkgs.lib.callPackageWith (pkgs // { localPackages = packages; });
+  packages = {
+    balc = callPackage ./balc/package.nix {};
+    fight = callPackage ./fight/package.nix {};
+    fuiska = callPackage ./fuiska/package.nix {};
+    hue = callPackage ./hue/package.nix {};
+    rbld = callPackage ./rbld/package.nix {};
+    revive = callPackage ./revive/package.nix {};
+    unify = callPackage ./unify/package.nix {};
+    writeFishApplication = callPackage ./writeFishApplication/package.nix {};
+  };
+
+in packages

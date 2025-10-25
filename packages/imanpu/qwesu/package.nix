2e82a3cc81379c32229f8a5096ddcf4d2d21107f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-25 16:33:47 -0400

    imanpu/qwesu: add Channel support
    As always, here's your government mandated regex explainer. We'll use
    this example URL:
    https://releases.nixos.org/nixos/unstable/nixos-25.11pre882227.01f116e4df6a/nixexprs.tar.xz
    
    And, here's the full regex if you don't want to keep checking back:
    https:\/\/releases\.nixos\.org\/(((?:nixos\/)?[^\/]*\/[^-]*-\d+\.\d+)[^\/]*)
    
    First, we grab the beginning of the url with
    `https:\/\/releases\.nixos\.org\/`. If you can't handle dot escaping,
    you won't like what's coming.
    
    Next, we start a big capturing group. This will contain the "key" of the
    current revision, so we can compare it to the key we get from querying
    the s3 db later. It'll look something like:
    `nixos/unstable/nixos-25.11pre882227.01f116e4df6a`
    
    We then start a second capturing group. This will be within the key one,
    and will contain the prefix that we'll be using to search the S3
    database, not all the revision stuff at the end. It'll look something
    like: `nixos/unstable/nixos-25.11`.
    
    Now, need to parse the next few slash-separated strings. In a perfect
    world, we could just match for slash-suffixed strings - but
    `nixpkgs-unstable` channels will look like `nixpkgs/nixpkgs-25.11`. As a
    dirty workaround, we match for an optional `nixos/` with `(?:nixos\/)?`.
    Finally, we match for one slash-suffixed string with `[^\/]*\/`.
    
    Next, we need to include the actual channel name. On unstable branches,
    the channel name is followed by `pre` - on stable branches, it's
    followed by a period. Rather than doing conditional logic here, we just
    assume the channel will follow basic nixos versioning rules. Start with
    everything until a dash with `[^-]*`, then a literal `-`, and finally
    two numbers separated by a period with `\d+\.\d+`.
    
    At this point, we now end the prefix capturing group. We're still in the
    key capturing group, though - and we need to match all the hash
    nonsense. This is actually really easy - just match until the slash with
    `[^\/]*`, and end the capturing group. We're done!

--- packages/imanpu/qwesu/package.nix
+++ packages/imanpu/qwesu/package.nix
@@ -4,7 +4,7 @@ localPackages.writeFishApplication {
   name = "qwesu"; # Query Whether Each Source Updated
 
   runtimeInputs = builtins.attrValues {
-    inherit (pkgs) jq git;
+    inherit (pkgs) jq git xq-xml;
   };
 
   text = builtins.readFile ./qwesu.fish;
--- packages/imanpu/qwesu/qwesu.fish
+++ packages/imanpu/qwesu/qwesu.fish
@@ -35,6 +35,27 @@ switch $type
         if [ "$new_hash" != "$old_hash" ]
             echo $name
         end
+    case Channel
+        # We do some regex magic on the current url in the lockfile to access:
+        # 1. the key that this revision _would_ have in the s3 database
+        # 2. the channel we're pointing to. s3 caps requests at 1000 objects, so
+        # to stay under 1000, we only query for objects on the current channel.
+        # We can't use the `.name` field, since it can sometimes look like
+        # `nixos-unstable`, and we really need to search for the currently
+        # unreleased version, a la `nixos-25.11`
+        set pattern 'https:\/\/releases\.nixos\.org\/(((?:nixos\/)?[^\/]*\/[^-]*-\d+\.\d+)[^\/]*)'
+        set groups (echo $data | jq -r ".url" | string match --regex $pattern -g)
+        set old_key $groups[1]
+        set channel $groups[2]
+
+        # Query hydra's s3 bucket for all the objects on the current channel,
+        # then access the most recent one. This is a horrible, dirty, no-good,
+        # very bad hack. Please, help me find a way to kill this.
+        set contents (curl -sS "https://nix-releases.s3.amazonaws.com/?delimiter=/&prefix=$channel")
+        set new_key (echo $contents | xq -j | jq -r '.ListBucketResult.Contents | sort_by(.LastModified) | .[-1] | .Key')
+        if [ "$old_key" != "$new_key" ]
+            echo $name
+        end
     case '*'
         echo "(Skipping $name of type $type)"
         return 0

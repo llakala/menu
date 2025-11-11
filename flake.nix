cf251a3032cad42a6606cfb8db823f25c1752e86
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-11 15:37:13 -0500

    flake: update flake.lock

--- flake.nix
+++ flake.nix
@@ -10,24 +10,19 @@
 
     forAllSystems = function: lib.genAttrs
       [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]
-      (system: function nixpkgs.legacyPackages.${system});
+      (system: function nixpkgs.legacyPackages.${system} system);
   in {
     # Stored in a separate file, if you'd prefer to be flakeless
-    legacyPackages = forAllSystems (pkgs: import ./packages/default.nix { inherit pkgs; });
+    legacyPackages = forAllSystems (pkgs: _:import ./packages/default.nix { inherit pkgs; });
 
-    devShells = forAllSystems (pkgs: {
+    devShells = forAllSystems (pkgs: system: {
       default = pkgs.mkShellNoCC {
         packages = builtins.attrValues {
-          inherit (self.legacyPackages.${pkgs.system})
+          inherit (self.legacyPackages.${system})
             fuiska
             rbld
             imanpu
             unify;
-
-          # Packages for devshell UX. nixpkgs provides gitMinimal by default these
-          # days, which doesn't provide stuff like `git send-email`, which is used by
-          # a contributor for sending patches without Github.
-          inherit (pkgs) git;
         };
       };
       }

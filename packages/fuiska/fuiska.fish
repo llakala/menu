0b46e24cdc646d57fe3fe02cfd582cef90a687ae
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-12-03 15:07:23 -0500

    fuiska, hue: improve directory logic
    New behavior here. If an arg for `-d` is passed, we use it, and error if
    it's invalid. However, if you don't pass a directory, we now use the
    current dir, _if_ it passes hue tests. We only fall back to the default
    dir if the current dir is invalid.
    
    We also actually print the dir that's being used on execution, with some
    pretty logic, so `../` gets expanded, and `/home/user/foo/bar` gets
    turned into `~/foo/bar`.

--- packages/fuiska/fuiska.fish
+++ packages/fuiska/fuiska.fish
@@ -1,27 +1,40 @@
 #! /usr/bin/env fish
 
-# Equivalent of "${UNIFY_DIRECTORY:/etc/nixos}" in bash
-set directory (revive $UNIFY_DIRECTORY "/etc/nixos")
-
-set -l option1 (fish_opt --required --short d --long directory)
-
-set options $option1
+# Override value with `-d` / `--directory`
+set options (fish_opt --required-val --short d --long directory)
 argparse $options -- $argv
 
-# Override value with `-d` / `--directory`
+set -e directory
+# Use directory from flag if used
 if set -q _flag_directory
-    set directory $_flag_directory
+    set directory (realpath -s $_flag_directory)
+    hue $directory || exit
+else if set -q UNIFY_DIRECTORY
+    # If there's a default, use current dir if it passes `hue`, or fall back on default
+    set directory $PWD
+    if not hue $directory > /dev/null
+        echo "Current dir invalid, using default directory"
+        set directory (realpath -s $UNIFY_DIRECTORY)
+        hue $directory || exit
+    end
+else
+    # No default, no flag - use current dir
+    set directory $PWD
+    hue $directory || exit
 end
 
-# Fail early here if hue says bad, since `set -e` doesn't exist
-hue $directory || exit
-cd $directory
+# We do the check here, so that if your default dir is the current one, or you
+# pass `-d .`, you stil lget the right result
+if [ $directory = $PWD ]
+    echo "Inputs that need updating for current directory:"
+else
+    set formatted_dir (string replace -r "^$HOME" "~" $directory)
+    echo "Inputs that need updating for $formatted_dir:"
+end
 
 # We use "$()" to save as a multiline string
-set full_contents "$(cat flake.lock)"
+set full_contents "$(cat $directory/flake.lock)"
 set names (echo $full_contents | jq -r ".nodes.root.inputs | keys[]")
-
-echo "Inputs that need updating:"
 
 # We parallelize checking the input via `&`
 for name in $names
--- packages/hue/hue.fish
+++ packages/hue/hue.fish
@@ -9,22 +9,27 @@ else if [ (count $argv) -gt 1 ]
     exit 1
 end
 
-set DIRECTORY $argv[1]
+set dir $argv[1]
+set formatted_dir (string replace -r "^$HOME" "~" $dir) # Prettier for printing
 
-if [ ! -d $DIRECTORY ]
-    echo "Directory `$DIRECTORY` does not exist, and can't be used for rebuilding."
+if [ ! -d $dir ]
+    echo "Directory `$formatted_dir` does not exist, and can't be used for rebuilding."
     exit 1
 end
 
-cd "$DIRECTORY" # So we can test on the current directory
+cd "$dir" # So we can test on the current directory
 
 if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    echo "Directory `$DIRECTORY` isn't a git repo, and was expected to be one."
+    echo "Directory `$formatted_dir` isn't a git repo, and was expected to be one."
     exit 1
 end
 
 if [ ! -f flake.nix ]
-    echo "Directory `$DIRECTORY` doesn't contain a `flake.nix` file."
+    echo "Directory `$formatted_dir` doesn't contain a `flake.nix` file."
+    exit 1
+end
+if [ ! -f flake.lock ]
+    echo "Directory `$formatted_dir` doesn't contain a `flake.lock` file."
     exit 1
 end
 
